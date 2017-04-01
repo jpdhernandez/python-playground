6adb139708325fe0e2ab2ace13ba612634c05877
jpdhernandez <jpdhernandez@gmail.com>
2017-04-01 19:44:53 -0400

    Try array operations

---
+++ python-for-data-analysis/numpy/array-operations.py
@@ -0,0 +1,87 @@
+
+# coding: utf-8
+
+# Numpy Array Operations
+# - array with array
+# - array with scalars
+# - universal array functions
+
+# In[1]:
+
+import numpy as np
+
+
+# In[2]:
+
+arr = np.arange(0,11)
+
+
+# In[4]:
+
+arr + arr
+
+
+# In[5]:
+
+arr - arr
+
+
+# In[6]:
+
+arr * arr
+
+
+# In[7]:
+
+arr + 100
+
+
+# In[8]:
+
+arr - 100
+
+
+# In[10]:
+
+arr / arr
+
+
+# In[11]:
+
+1 / arr
+
+
+# In[12]:
+
+arr ** 2
+
+
+# In[13]:
+
+np.sqrt(arr)
+
+
+# In[14]:
+
+np.exp(arr)
+
+
+# In[15]:
+
+np.max(arr)
+
+
+# In[16]:
+
+np.min(arr)
+
+
+# In[17]:
+
+np.sin(arr)
+
+
+# In[18]:
+
+np.log(arr)
+
